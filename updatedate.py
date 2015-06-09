10c571d8db7ccc9cfde9abbbb8d6627f7f8a8ff1
Thomas Hodgson <hello@twshodgson.co.uk>
2015-06-08 23:31:36 -0500

    Tweaked the command for date

--- updatedate.py
+++ updatedate.py
@@ -7,7 +7,6 @@ import sublime_plugin
 
 class UpdateDateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # The below will work on Windows, and doesn't have a leading '0'
-        today = datetime.datetime.now().strftime('%d %B %Y').lstrip('0')
+        today = datetime.datetime.now().strftime('%-e %B %Y').lstrip('0')
         region = self.view.find('date:\\s*\\d{1,2}\\s\\w*\\s*\\d{4}', 0)
         self.view.replace(edit, region, 'date: {}'.format(today))
